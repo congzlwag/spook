13fea4a05cd164cd5d8db96194cda126b97ce2d4
congzlwag <congzlwag@163.com>
2022-01-18 20:37:35 -0800

    Start functionalizing spook for VMI

---
+++ vmi_special.py
@@ -0,0 +1,9 @@
+import numpy as np
+from .lin_solve import SpookLinSolve
+import scipy.sparse as sps
+from .utils import laplacian_square_S
+
+from pbasex import loadG, pbasex
+
+def photon_freq_res_VMI(vls_spec_dict, processed_quad_images_dict, gData):
+	pass
