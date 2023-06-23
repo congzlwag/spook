f430439116a29e0f142262da216cbce9f848de3a
Jun Wang <junwang9@stanford.edu>
2023-06-23 15:39:49 -0700

    Add version information.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from . import spook
 from .spook import *
+from .__version__ import __version__
--- spook/__init__.py
+++ spook/__init__.py
@@ -2,4 +2,5 @@ from .base import SpookBase
 from .lin_solve import SpookLinSolve
 from .quad_program import SpookPosL1,SpookPosL2,SpookL1
 from . import utils
+from ..__version__ import __version__
 # from spook.vmi_special import PhotonFreqResVMI
