7c38aba75b814129665a935267412066fe639205
Jun Wang <junwang9@stanford.edu>
2023-06-23 16:48:03 -0700

    Try another way of versioning

--- spook/__init__.py
+++ spook/__init__.py
@@ -2,5 +2,7 @@ from .base import SpookBase
 from .lin_solve import SpookLinSolve
 from .quad_program import SpookPosL1,SpookPosL2,SpookL1
 from . import utils
-from .__version__ import __version__
+# from . __version__ import __version__
 # from spook.vmi_special import PhotonFreqResVMI
+
+__version__ = '0.9.4'
